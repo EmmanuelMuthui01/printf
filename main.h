51c11e47c90852266a4fcf8b71cd36d8883dcba1
Emmiechege <emilywanjachege@gmail.com>
2023-08-23 13:10:49 -0700

    function prototypes

--- main.h
+++ main.h
@@ -8,4 +8,9 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 
+/* helper functions */
+int print_string(char *str);
+int handle_string(va_list args);
+int handle_char(va_list args);
+
 #endif
